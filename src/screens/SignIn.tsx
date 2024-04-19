e48b9016d17c0537fec0d25c766b0354b5f0b4eb
dev.tronghieu <dev.ngtronghieu@gmail.com>
2024-04-19 22:42:22 +0700

    ui: change color scheme

--- src/screens/SignIn.tsx
+++ src/screens/SignIn.tsx
@@ -1,6 +1,10 @@
+import strawberry from "../assets/strawberry.svg";
+
 const SignInScreen = () => {
   return (
     <div className="extension-container">
+      <img src={strawberry} alt="strawberry" className="w-48 h-48" />
+
       <p className="text-red-500">Sign-in page</p>
     </div>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,29 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
+    colors: {
+      primary: {
+        100: "#FCEBEE",
+        200: "#F29AAA",
+        300: "#EF5385",
+        400: "#C33160",
+      },
+      secondary: {
+        100: "#A6D9DA",
+        200: "#267578",
+      },
+      tertiary: {
+        100: "#FFDFBE",
+        200: "#C69881",
+      },
+      gray: {
+        100: "#E8E8E8",
+        200: "#D2D2D2",
+        300: "#A5A5A5",
+        400: "#777777",
+        500: "#4A4A4A",
+      },
+    },
     extend: {},
   },
   plugins: [],
