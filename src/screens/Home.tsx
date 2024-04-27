003579ce90bf282de2a44b1d08b33fdd5758a971
dev.tronghieu <dev.ngtronghieu@gmail.com>
2024-04-27 23:42:51 +0700

    chore: fetch token accounts

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,8 +1,20 @@
+import { fetchOnchainData } from "@engine/index";
 import { appState } from "@state/index";
+import { useEffect } from "react";
 import { useSnapshot } from "valtio";
 
 const HomeScreen = () => {
   const { keypair } = useSnapshot(appState);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      if (!keypair) return;
+      await fetchOnchainData(keypair.publicKey);
+    };
+
+    fetchData().catch(console.error);
+  }, [keypair]);
+
   return (
     <div>
       <p className="text-red-500">Hello, Berry Wallet</p>
