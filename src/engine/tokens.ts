75a3f8b4055138de8c737c955a8d27c4aff65b0a
dev.tronghieu <dev.ngtronghieu@gmail.com>
2024-04-28 15:44:04 +0700

    chore: backup metadata for usdc-dev

--- src/engine/tokens.ts
+++ src/engine/tokens.ts
@@ -5,6 +5,51 @@ import { WRAPPED_SOL_MINT } from "./constants";
 import { Token, TokenMetadata } from "./types";
 import { getConnection } from "./connection";
 
+const getBackupMetadata = (mint: string): TokenMetadata | undefined => {
+  switch (mint) {
+    // USDC Dev Coin
+    case "Gh9ZwEmdLJ8DscKNTkTqPbNwLNNBjuSzaG9Vp2KGtKJr":
+      return {
+        name: "USD Dev Coin",
+        symbol: "USDC",
+        logo: "https://statics.solscan.io/cdn/imgs/s60?ref=68747470733a2f2f7261772e67697468756275736572636f6e74656e742e636f6d2f736f6c616e612d6c6162732f746f6b656e2d6c6973742f6d61696e2f6173736574732f6d61696e6e65742f45506a465764643541756671535371654d32714e31787a7962617043384734774547476b5a777954447431762f6c6f676f2e706e67",
+      };
+    default:
+      return;
+  }
+};
+
+const fetchTokenMetadata = async (mintAddress: string) => {
+  const connection = getConnection();
+
+  const mint = new PublicKey(mintAddress);
+
+  const [pda] = PublicKey.findProgramAddressSync(
+    [Buffer.from("metadata"), METADATA_PROGRAM_ID.toBuffer(), mint.toBuffer()],
+    METADATA_PROGRAM_ID,
+  );
+
+  try {
+    const onchainData = await Metadata.fromAccountAddress(connection, pda);
+    const metadata: TokenMetadata = {
+      name: onchainData.data.name.replaceAll("\u0000", ""),
+      symbol: onchainData.data.symbol.replaceAll("\u0000", ""),
+      logo: "",
+    };
+
+    const onchainJson = onchainData.data.uri.replaceAll("\u0000", "");
+    if (onchainJson.startsWith("https://")) {
+      const response = await fetch(onchainJson);
+      const offChainData = await response.json();
+      metadata.logo = offChainData.image;
+    }
+
+    return metadata;
+  } catch (error) {
+    return getBackupMetadata(mintAddress);
+  }
+};
+
 export const fetchTokens = async (pubkey: PublicKey) => {
   const connection = getConnection();
 
@@ -41,37 +86,7 @@ export const fetchTokens = async (pubkey: PublicKey) => {
   // Token Metadata
   const metadataPromises: Promise<TokenMetadata | undefined>[] = [];
   tokens.forEach((token) => {
-    const fetchTokenMetadata = async () => {
-      const mint = new PublicKey(token.mint);
-
-      const [pda] = PublicKey.findProgramAddressSync(
-        [Buffer.from("metadata"), METADATA_PROGRAM_ID.toBuffer(), mint.toBuffer()],
-        METADATA_PROGRAM_ID,
-      );
-
-      try {
-        const onchainData = await Metadata.fromAccountAddress(connection, pda);
-        const metadata: TokenMetadata = {
-          name: onchainData.data.name.replaceAll("\u0000", ""),
-          symbol: onchainData.data.symbol.replaceAll("\u0000", ""),
-          logo: "",
-        };
-
-        const onchainJson = onchainData.data.uri.replaceAll("\u0000", "");
-        if (onchainJson.startsWith("https://")) {
-          const response = await fetch(onchainJson);
-          const offChainData = await response.json();
-          metadata.logo = offChainData.image;
-        }
-
-        return metadata;
-      } catch (error) {
-        console.error(`Error fetching metadata for token ${token.mint}: ${error}`);
-        return undefined;
-      }
-    };
-
-    metadataPromises.push(fetchTokenMetadata());
+    metadataPromises.push(fetchTokenMetadata(token.mint));
   });
   const metadataList = await Promise.all(metadataPromises);
   metadataList.forEach((metadata, index) => {
