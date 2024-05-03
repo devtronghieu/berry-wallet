f53e32ff14ebe0e6a4453ab2d8fdffac1ce65291
ltminhthu <ltminhthu.209@gmail.com>
2024-05-03 12:26:28 +0700

    remove extension container

--- src/screens/Receive/index.tsx
+++ src/screens/Receive/index.tsx
@@ -13,24 +13,22 @@ const Receive: FC = () => {
   };
 
   return (
-    <div className="extension-container bg-secondary-200">
-      <div className="flex flex-col justify-center items-center gap-5 w-full h-full px-5">
-        <div className="bg-primary-200 p-4 w-fit rounded-2xl">
-          <QRCode value={keypair?.publicKey.toString() as string} size={200} bgColor="#F7C3CC" fgColor="#EF5385" />
+    <div className="flex flex-col justify-center items-center gap-5 w-full h-full px-5">
+      <div className="bg-primary-200 p-4 w-fit rounded-2xl">
+        <QRCode value={keypair?.publicKey.toString() as string} size={200} bgColor="#F7C3CC" fgColor="#EF5385" />
+      </div>
+
+      <div className="flex justify-between items-center bg-primary-200 p-4 rounded-2xl gap-4 w-full py-2">
+        <img src={solIcon} />
+
+        <div className="flex flex-1 flex-col">
+          <p className="text-secondary-500 font-bold">Account 1</p>
+          <p className="text-secondary-500">A4sd...rkjF</p>
         </div>
 
-        <div className="flex justify-between items-center bg-primary-200 p-4 rounded-2xl gap-4 w-full py-2">
-          <img src={solIcon} />
-
-          <div className="flex flex-1 flex-col">
-            <p className="text-secondary-500 font-bold">Account 1</p>
-            <p className="text-secondary-500">A4sd...rkjF</p>
-          </div>
-
-          <button className="flex justify-center items-center bg-primary-100 rounded-full w-8 h-8" onClick={handleCopy}>
-            <CopyIcon />
-          </button>
-        </div>
+        <button className="flex justify-center items-center bg-primary-100 rounded-full w-8 h-8" onClick={handleCopy}>
+          <CopyIcon />
+        </button>
       </div>
     </div>
   );
