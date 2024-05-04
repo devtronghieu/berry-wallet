24caef46a77dde03ecdd38108f547802927dcfdf
ltminhthu <ltminhthu.209@gmail.com>
2024-05-04 12:17:15 +0700

    components

---
+++ src/screens/History/HistoryItem/NFTHistoryItem.tsx
@@ -0,0 +1,43 @@
+import { FC, useMemo } from "react";
+import { NFTHistoryItemProps, TransactionType } from "../internal";
+import { ArrowDown, ArrowUp } from "@/icons";
+import HistoryItem from ".";
+
+const NFTHistoryItem: FC<NFTHistoryItemProps> = ({ nftImage, nftName, transactionType, receiver, sender }) => {
+  const Icon = useMemo(() => {
+    return (
+      <div className="flex items-end">
+        <img src={nftImage} alt="icon" className="h-10 w-10 rounded-full border-2 border-primary-200" />
+        <div className="flex justify-center items-center w-5 h-5 bg-secondary-100 rounded-full border-2 border-primary-200 ml-[-12px]">
+          {transactionType === TransactionType.SEND ? (
+            <ArrowUp size={16} color="#EF5385" />
+          ) : (
+            <ArrowDown size={16} color="#267578" />
+          )}
+        </div>
+      </div>
+    );
+  }, [transactionType, nftImage]);
+
+  const Address = useMemo(() => {
+    if (transactionType === TransactionType.SEND) {
+      return (
+        <div className="flex flex-1 flex-col items-start">
+          <p className="text-primary-400 font-semibold">Sent {nftName}</p>
+          <p className="text-primary-400">{receiver}</p>
+        </div>
+      );
+    } else if (transactionType === TransactionType.RECEIVE) {
+      return (
+        <div className="flex flex-1 flex-col items-start">
+          <p className="text-primary-400 font-semibold">Received {nftName}</p>
+          <p className="text-primary-400">{sender}</p>
+        </div>
+      );
+    }
+  }, [transactionType, nftName, sender, receiver]);
+
+  return <HistoryItem Icon={Icon} Address={Address} />;
+};
+
+export default NFTHistoryItem;
--- src/screens/History/HistoryItem/TokenHistoryItem.tsx
+++ src/screens/History/HistoryItem/TokenHistoryItem.tsx
@@ -1,26 +1,10 @@
 import { ArrowDown, ArrowUp } from "@/icons";
 import { FC, useMemo } from "react";
+import { TokenHistoryItemProps, TransactionType } from "../internal";
+import HistoryItem from ".";
 
-export enum TransactionType {
-  SEND = "SEND",
-  RECEIVE = "RECEIVE",
-  SWAP = "SWAP",
-}
-
-export interface HistoryItemProps {
-  type: TransactionType;
-  amount: number;
-  tokenImage: string;
-  tokenName: string;
-  sender?: string;
-  receiver?: string;
-  receivedAmount?: number;
-  receivedTokenImage?: string;
-  receivedTokenName?: string;
-}
-
-const HistoryItem: FC<HistoryItemProps> = ({
-  type,
+const TokenHistoryItem: FC<TokenHistoryItemProps> = ({
+  transactionType,
   amount,
   tokenImage,
   tokenName,
@@ -31,7 +15,7 @@ const HistoryItem: FC<HistoryItemProps> = ({
   receivedTokenName,
 }) => {
   const Icon = useMemo(() => {
-    if (type === TransactionType.SWAP) {
+    if (transactionType === TransactionType.SWAP) {
       return (
         <div className="flex">
           <img src={tokenImage} alt="icon" className="h-8 w-8 rounded-full border-2 border-primary-200" />
@@ -47,7 +31,7 @@ const HistoryItem: FC<HistoryItemProps> = ({
         <div className="flex items-end">
           <img src={tokenImage} alt="icon" className="h-10 w-10 rounded-full border-2 border-primary-200" />
           <div className="flex justify-center items-center w-5 h-5 bg-secondary-100 rounded-full border-2 border-primary-200 ml-[-12px]">
-            {type === TransactionType.SEND ? (
+            {transactionType === TransactionType.SEND ? (
               <ArrowUp size={16} color="#EF5385" />
             ) : (
               <ArrowDown size={16} color="#267578" />
@@ -56,67 +40,61 @@ const HistoryItem: FC<HistoryItemProps> = ({
         </div>
       );
     }
-  }, [type, tokenImage, receivedTokenImage]);
+  }, [transactionType, tokenImage, receivedTokenImage]);
 
   const Address = useMemo(() => {
-    if (type === TransactionType.SWAP) {
+    if (transactionType === TransactionType.SWAP) {
       return (
-        <div className="flex flex-1 flex-col">
-          <p className="text-tertiary-300 font-semibold">Swapped</p>
-          <p className="text-tertiary-100">Jupiter</p>
+        <div className="flex flex-1 flex-col items-start">
+          <p className="text-primary-400 font-semibold">Swapped</p>
+          <p className="text-secondary-500">Jupiter</p>
         </div>
       );
-    } else if (type === TransactionType.SEND) {
+    } else if (transactionType === TransactionType.SEND) {
       return (
-        <div className="flex flex-1 flex-col">
+        <div className="flex flex-1 flex-col items-start">
           <p className="text-primary-400 font-semibold">Sent</p>
-          <p className="text-primary-300">{receiver}</p>
+          <p className="text-primary-400">{receiver}</p>
         </div>
       );
-    } else if (type === TransactionType.RECEIVE) {
+    } else if (transactionType === TransactionType.RECEIVE) {
       return (
-        <div className="flex flex-1 flex-col">
+        <div className="flex flex-1 flex-col items-start">
           <p className="text-secondary-500 font-semibold">Received</p>
-          <p className="text-secondary-300">{sender}</p>
+          <p className="text-secondary-500">{sender}</p>
         </div>
       );
     }
-  }, [sender, receiver, type]);
+  }, [sender, receiver, transactionType]);
 
   const TotalAmount = useMemo(() => {
-    if (type === TransactionType.SWAP) {
+    if (transactionType === TransactionType.SWAP) {
       return (
         <div>
-          <div className="text-primary-400">
+          <p className="text-primary-400  font-semibold text-end">
             -{amount} {tokenName}
-          </div>
-          <div className="text-secondary-500">
+          </p>
+          <p className="text-secondary-500  font-semibold text-end">
             +{receivedAmount} {receivedTokenName}
-          </div>
+          </p>
         </div>
       );
-    } else if (type === TransactionType.SEND) {
+    } else if (transactionType === TransactionType.SEND) {
       return (
-        <p className="flex text-primary-400">
+        <p className="flex text-primary-400  font-semibold text-end">
           -{amount} {tokenName}
         </p>
       );
-    } else if (type === TransactionType.RECEIVE) {
+    } else if (transactionType === TransactionType.RECEIVE) {
       return (
-        <p className="flex text-secondary-500">
+        <p className="flex text-secondary-500  font-semibold text-end">
           +{amount} {tokenName}
         </p>
       );
     }
-  }, [type, amount, tokenName, receivedAmount, receivedTokenName]);
+  }, [transactionType, amount, tokenName, receivedAmount, receivedTokenName]);
 
-  return (
-    <div className="flex justify-between bg-primary-200">
-      {Icon}
-      {Address}
-      {TotalAmount}
-    </div>
-  );
+  return <HistoryItem Icon={Icon} Address={Address} Amount={TotalAmount} />;
 };
 
-export default HistoryItem;
+export default TokenHistoryItem;
