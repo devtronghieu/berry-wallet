064ca8b2b021d18011c7fc4137d7cc3b76451aa7
ltminhthu <ltminhthu.209@gmail.com>
2024-05-04 12:17:47 +0700

    history details bottom sheet

---
+++ src/components/TransactionInfo/index.tsx
@@ -0,0 +1,72 @@
+import { TransactionStatus, TransactionType } from "@screens/History/internal";
+import TransactionInfoItem from "./TransactionInfoItem";
+import { FC, useMemo } from "react";
+import { CorrectIcon } from "@/icons";
+import SeparatedLine from "@components/SeparatedLine";
+
+export interface TransactionInfoProps {
+  transactionType: TransactionType;
+  date: Date;
+  sender?: string;
+  receiver?: string;
+  status: TransactionStatus;
+  fee: number;
+}
+
+const TransactionInfo: FC<TransactionInfoProps> = ({ date, status, transactionType, receiver, sender, fee }) => {
+  const Address = useMemo(() => {
+    let title = "";
+    let value = "";
+    if (transactionType === TransactionType.SWAP) {
+      title = "Provider";
+      value = "Jupiter";
+    } else if (transactionType === TransactionType.SEND) {
+      title = "To";
+      value = receiver as string;
+    } else if (transactionType === TransactionType.RECEIVE) {
+      title = "From";
+      value = sender as string;
+    }
+
+    return <TransactionInfoItem title={title} value={value} />;
+  }, [transactionType, receiver, sender]);
+
+  const Date = useMemo(() => {
+    return <TransactionInfoItem title="Date" value={date.toDateString()} />;
+  }, [date]);
+
+  const Status = useMemo(() => {
+    return (
+      <TransactionInfoItem
+        title="Status"
+        value={
+          <div>
+            {status}
+            {status === TransactionStatus.SUCCESS ? <CorrectIcon /> : status === TransactionStatus.PENDING ? "..." : ""}
+          </div>
+        }
+      />
+    );
+  }, [status]);
+
+  const Fee = useMemo(() => {
+    return <TransactionInfoItem title="Fee" value={`${fee} SOL`} />;
+  }, [fee]);
+
+  return (
+    <div className="bg-primary-200 rounded-3xl">
+      {Address}
+      <SeparatedLine />
+
+      {Date}
+      <SeparatedLine />
+
+      {Status}
+      <SeparatedLine />
+
+      {Fee}
+    </div>
+  );
+};
+
+export default TransactionInfo;
---
+++ src/screens/History/HistoryDetails/index.tsx
@@ -0,0 +1,11 @@
+export interface HistoryDetailsProps {
+    
+}
+
+const HistoryDetails = () => {
+    return (
+        
+    );
+}
+
+export default HistoryDetails;
