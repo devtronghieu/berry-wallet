1e2c0f3182349c29fa14337aa905ff277f4660a9
dev.tronghieu <dev.ngtronghieu@gmail.com>
2024-05-05 22:11:59 +0700

    chore: fix css loading the whole app & cache pubkey

--- src/extension/background.ts
+++ src/extension/background.ts
@@ -3,23 +3,29 @@ import { Channel, DAppPayload, Event } from "@messaging/types";
 
 import { openPopup } from "./utils";
 
-console.log("Background script loaded");
+type ConnectOptions = { onlyIfTrusted?: boolean | undefined } | undefined;
 
 const chromeKernel = new ChromeKernel(Channel.Background);
+
+let currentPublicKey: string | null = null;
 
 chromeKernel.handleRequest = async (request) => {
   const payload = request.payload as DAppPayload;
 
   switch (payload.event) {
     case Event.Connect: {
+      const options = payload.data as ConnectOptions;
+      if (options?.onlyIfTrusted && currentPublicKey) {
+        return currentPublicKey;
+      }
+
       const resolveId = crypto.randomUUID();
       openPopup(payload.event, resolveId);
       const response = await chromeKernel.waitForResolve({
         id: resolveId,
-        contextData: {
-          hello: "world",
-        },
+        contextData: payload.data,
       });
+      currentPublicKey = response.payload as string;
       return response.payload;
     }
 
--- src/screens/Request/index.tsx
+++ src/screens/Request/index.tsx
@@ -1,10 +1,8 @@
-import "./index.css";
-
 import { Outlet } from "react-router-dom";
 
 const RequestScreen = () => {
   return (
-    <div>
+    <div className="w-screen h-screen bg-primary-100">
       <h1>Requests</h1>
 
       <Outlet />
