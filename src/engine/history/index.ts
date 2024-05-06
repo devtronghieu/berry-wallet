aafb5bfc9a3c7cf0fba37ff5bfd5e1d55cd3e58e
ltminhthu <ltminhthu.209@gmail.com>
2024-05-06 17:07:45 +0700

    engine: history

---
+++ src/engine/history/index.ts
@@ -0,0 +1,56 @@
+import { TOKEN_PROGRAM_ID } from '@solana/spl-token';
+import { ParsedInstruction, ParsedTransactionWithMeta, PublicKey } from '@solana/web3.js';
+
+import { getConnection } from './../connection';
+import { transferTokenTransationDetail } from './transferTransaction/transfer';
+import { transferCheckedTransationDetail } from './transferTransaction/transferChecked';
+
+export const connection = getConnection();
+
+export const getSignatures = async (address: PublicKey, limit: number = 1000) => {
+    const confirmedSignatureInfos = await connection.getSignaturesForAddress(address, {limit});
+    const signatures = confirmedSignatureInfos.map((info) => info.signature);
+
+    return signatures;
+};
+
+export const getTransactionDetail = async (transaction: ParsedTransactionWithMeta) => {
+    const instructions = transaction.transaction.message.instructions;
+    if (isSwapTransaction(instructions as ParsedInstruction[])) return null;
+
+    const specificInstruction = getSpecificInstructionByProgramId(instructions as ParsedInstruction[], TOKEN_PROGRAM_ID);
+
+    if (!specificInstruction) return null;
+
+    const signature = transaction.transaction.signatures[0];
+
+    let transactionDetail;
+    if (specificInstruction.parsed.type === "transferChecked") {
+        transactionDetail = await transferCheckedTransationDetail(specificInstruction, transaction.meta, transaction.blockTime as number, signature);
+    } else if (specificInstruction.parsed.type === "transfer") {
+        transactionDetail = await transferTokenTransationDetail(specificInstruction, transaction.meta, transaction.blockTime as number, signature);
+    }
+    
+    return transactionDetail;
+};
+
+export const getSpecificInstructionByProgramId = (instructions: ParsedInstruction[] , programId: PublicKey) => {
+    const specificInstruction = instructions.find((instruction) => instruction.programId.equals(programId));
+    
+    return specificInstruction;
+};
+
+export const isSwapTransaction = (instructions: ParsedInstruction[]) => {
+    const isSwap = instructions.some((instruction) => instruction.programId.equals(new PublicKey(import.meta.env.VITE_JUPITER_PROGRAM_ID)));
+
+    return isSwap;
+}
+
+export const getTransaction = async (signature: string) => {
+    const transaction = await connection.getParsedTransaction(signature, {maxSupportedTransactionVersion: 2});
+    if (!transaction) return null;
+
+    const transactionDetail = await getTransactionDetail(transaction);
+    return transactionDetail;
+};
+
---
+++ src/engine/history/transferTransaction/transfer.ts
@@ -0,0 +1,60 @@
+import { fetchTokenMetadata, getLocalToken, getTokenDecimalsByMint } from "@engine/tokens";
+import { getAccount } from "@solana/spl-token";
+import { ParsedInstruction, ParsedTransactionMeta, PublicKey } from "@solana/web3.js";
+import { appState } from "@state/index";
+
+import { connection } from "..";
+import { TokenType, TransactionStatus, TransactionType, TransferTransaction } from "../type";
+
+export const getAmount = async (instruction: ParsedInstruction, decimals: number) => {    
+    return instruction.parsed.info.amount / Math.pow(10, decimals);
+};
+
+export const getAccountInfo = async (pubkey: PublicKey) => {
+    const account = await getAccount(connection, pubkey);
+    const owner = account.owner.toBase58();
+    const mint = account.mint.toBase58();
+
+    return {owner, mint};
+};
+
+export const transferTokenTransationDetail = async (instruction: ParsedInstruction, meta: ParsedTransactionMeta | null, blockTime: number, signature: string) => {
+    const account = await getAccountInfo(new PublicKey(instruction.parsed.info.destination));
+    const receiver = account.owner;
+    const sender = instruction.parsed.info.authority;
+    let token = await getLocalToken(account.mint);
+    if (!token) {
+        const mint = account.mint;
+        const decimals = await getTokenDecimalsByMint(mint);
+        const metadata = await fetchTokenMetadata(mint).catch(() => undefined);
+        token = {
+            pubkey: new PublicKey(mint),
+            mint,
+            owner: "",
+            amount: "0",
+            decimals,
+            metadata: metadata,
+        }
+    }
+
+    const amount = await getAmount(instruction, token?.decimals || 0);
+    const date = new Date(blockTime * 1000);
+    const fee = (meta?.fee || 0) / Math.pow(10, 9);
+    const status = meta?.err ? TransactionStatus.FAILED : TransactionStatus.SUCCESS;
+    const transactionType = receiver === appState.keypair?.publicKey.toBase58() ? TransactionType.RECEIVE : TransactionType.SEND;
+
+    const transactionDetail: TransferTransaction = {
+        signature,
+        token,
+        amount,
+        date,
+        transactionType,
+        sender,
+        receiver,
+        tokenType: TokenType.TOKEN,
+        status,
+        fee,
+    };
+
+    return transactionDetail;
+}
