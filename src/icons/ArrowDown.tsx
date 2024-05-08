2d0f3e272c6cea3ce57e45b95ffc095682a2d5e7
dev.tronghieu <dev.ngtronghieu@gmail.com>
2024-05-08 15:13:08 +0700

    chore: convert dash-case to camelCase

--- src/icons/ArrowDown.tsx
+++ src/icons/ArrowDown.tsx
@@ -5,7 +5,7 @@ import { IconProps } from "./types";
 export const ArrowDownIcon: FC<IconProps> = ({ size = 20, color = "#267578" }) => {
   return (
     <svg width={size} height={size} viewBox="0 0 24 24" fill="none">
-      <path d="M12.2744 19.75V4.75" stroke={color} stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
+      <path d="M12.2744 19.75V4.75" stroke={color} strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" />
       <path
         d="M18.2988 13.7002L12.2748 19.7502L6.24976 13.7002"
         stroke={color}
